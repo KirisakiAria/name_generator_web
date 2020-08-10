35584875fcb7e8213afe009283846e71c3be423e
kirisakiaria <xiaoli350791904@hotmail.com>
2020-08-10 23:40:48 +0800

    开发服务页面

--- src/request/API.js
+++ src/request/API.js
@@ -2,7 +2,7 @@ const version = 'v1'
 const api_prefix = `/api/${version}`
 
 export default {
-  login: `${api_prefix}/service/privacypolicy`,
+  privacyPolicy: `${api_prefix}/service/privacypolicy`,
   terms: `${api_prefix}/service/terms`,
   usage: `${api_prefix}/usage`,
 }
